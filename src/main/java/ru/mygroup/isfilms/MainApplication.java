e9cbc9f5a0a069be1efadccecf42a8674274002a
shblkjo <daria_afanaseva2005@mail.ru>
2026-06-07 20:20:55 +0300

    Изменение названия окна

--- src/main/java/ru/mygroup/isfilms/MainApplication.java
+++ src/main/java/ru/mygroup/isfilms/MainApplication.java
@@ -12,7 +12,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 838, 654);
-        stage.setTitle("Hello!");
+        stage.setTitle("Система хранения и поиска фильмов");
         stage.setScene(scene);
         stage.show();
     }
